02172c203eacf148e8c9f976802b88c54d728bde
taiyme <53635909+taiyme@users.noreply.github.com>
2025-06-27 17:26:12 +0900

    chore: use @taiyme/vite-plugin-jsx-prune-classname

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import build from '@hono/vite-build/cloudflare-workers';
 import adapter from '@hono/vite-dev-server/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
+import jsxPruneClassName from '@taiyme/vite-plugin-jsx-prune-classname';
 import honox from 'honox/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-import jsxClassnameTrim from './vite-plugin-jsx-classname-trim';
-
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    jsxClassnameTrim(),
+    jsxPruneClassName(),
     honox({
       client: {
         input: [
